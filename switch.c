671fe050d1edc2255439694ec5abb30c2b67f543
Yogesh bhardwaj <128271891+yogesh0535@users.noreply.github.com>
2024-01-02 22:18:08 +0530

    Update switch.c

--- switch.c
+++ switch.c
@@ -1,93 +1,103 @@
-// 
-// #include<stdio.h>
-// int main(){
-    // int  day;
-    // scanf("%d",&day);
-// 
-    // switch(day){
-        // case 1:
-        // printf("day is monday");
-        // break;
-// 
-        // case 2:
-        // printf("day is tuesday");
-        // break;
-// 
-        // case 3:
-        // printf("day is wednesday");
-        // break;
-// 
-        // case 4:
-        // printf("day is thursday");
-        // break;
-// 
-        // case 5:
-        // printf("day is friday");
-        // break;
-    // 
-        // case 6:
-        // printf("day is saturday");
-        // break;
-// 
-        // case 7:
-        // printf("day is sunday");
-        // break;
-    //    
-    //    default:
-    //    printf("day is not exits above 7");
-// 
-    // return 0;
-// 
-    // }
-// }
 
-//   #include<stdio.h>
-//   int days(int day){
-    // switch(day){
-        // case 1:
-        // printf("day is monday");
-        // break;
-// 
-        // case 2:
-        // printf("day is tuesday");
-        // break;
-// 
-        // case 3:
-        // printf("day is wednesday");
-        // break;
-// 
-        // case 4:
-        // printf("day is thursday");
-        // break;
-// 
-        // case 5:
-        // printf("day is friday");
-        // break;
-    // 
-        // case 6:
-        // printf("day is saturday");
-        // break;
-// 
-        // case 7:
-        // printf("day is sunday");
-        // break;
-    //    
-    //    default:
-    //    printf("day is not exits above 7");
-    //   return 0;
-//  }
-// 
-// 
-// int main(){
-    // int day;
-    // printf("enter the day number");
-    // scanf("%d",&day);
-    // 
-    // int r=days(day);
-    // printf("%d",r);
-    // return 0;
-// }
+#include<stdio.h>
+int main(){
+    int  day;
+    scanf("%d",&day);
 
+    switch(day){
+        case 1:
+        printf("day is monday");
+        break;
+
+        case 2:
+        printf("day is tuesday");
+        break;
+
+        case 3:
+        printf("day is wednesday");
+        break;
+
+        case 4:
+        printf("day is thursday");
+        break;
+
+        case 5:
+        printf("day is friday");
+        break;
+    
+        case 6:
+        printf("day is saturday");
+        break;
+
+        case 7:
+        printf("day is sunday");
+        break;
+       
+       default:
+       printf("day is not exits above 7");
+
+    return 0;
+
+    }
+}
+
+
+
+  #include<stdio.h>
+  int days(int day){
+    switch(day){
+        case 1:
+        printf("day is monday");
+        break;
+
+        case 2:
+        printf("day is tuesday");
+        break;
+
+        case 3:
+        printf("day is wednesday");
+        break;
+
+        case 4:
+        printf("day is thursday");
+        break;
+
+        case 5:
+        printf("day is friday");
+        break;
+    
+        case 6:
+        printf("day is saturday");
+        break;
+
+        case 7:
+        printf("day is sunday");
+        break;
+       
+       default:
+       printf("day is not exits above 7");
+      return 0;
+ }
+
+int main(){
+    int day;
+    printf("enter the day number");
+    scanf("%d",&day);
+    
+    int r=days(day);
+    printf("%d",r);
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+      
 #include<stdio.h>
 int main(){
     int a,b,c;
@@ -114,64 +124,4 @@ int main(){
 
     return 0;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
