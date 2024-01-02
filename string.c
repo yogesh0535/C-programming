7299434476010b1af5888712bbec1eeffe330615
Yogesh bhardwaj <128271891+yogesh0535@users.noreply.github.com>
2024-01-02 22:09:18 +0530

    Update string.c

--- string.c
+++ string.c
@@ -1,66 +1,44 @@
-// #include<stdio.h>
-// int main(){
-    // char name[20];
-    // gets(name);             /* to input string*/
-    // puts(name);              /* to print strng*/
-    // return 0;
-// }
-
-                        
-                        //   length of string
-// #include<stdio.h>
-// #include<string.h>
-// int main(){
-        //  char success[30];
-        //  char hard_work[30];
-    // printf("enter the first string:\n");
-        // gets(hard_work);
-    // printf("enter the second string:\n");
-        //  gets(success);
-        //    char thought[40];
-    //    printf("length of arrays respectively\t\t");
-        //  printf("%d\n",strlen(success));  
-        //  printf("%d\n",strlen(hard_work));  
-        //  printf("joining of 2 arays\n");
-    //   puts(strcat(hard_work,success));
-    // 
-    //   printf("\n\n");
-// 
-            // printf("third arry copied from first and second\n\t");
-        // puts(strcpy(thought,(strcat(hard_work,success))));
-    //   printf("\n\n");
-            // int l=(strcmp(hard_work,success));
-            // if(l=0){
-            // printf("equal");
-            // }
-            // else{
-                // printf("not equal");
-            // }
-// 
-            //   char reverse=  strrev(success);
-        //    printf("%c",reverse);
-// 
-    // return 0;
-// } 
 #include<stdio.h>
 int main(){
-    
+    char name[20];
+    gets(name);             /* to input string*/
+    puts(name);              /* to print strng*/
     return 0;
 }
 
-                
+                        
+                        //   length of string
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char success[30];
+    char hard_work[30];
+    printf("enter the first string:\n");
+    gets(hard_work);
+    printf("enter the second string:\n");
+    gets(success);
+    char thought[40];
+    printf("length of arrays respectively\t\t");
+    printf("%d\n",strlen(success));  
+    printf("%d\n",strlen(hard_work));  
+    printf("joining of 2 arays\n");
+    puts(strcat(hard_work,success));
+    
+    printf("\n\n");
 
+    printf("third arry copied from first and second\n\t");
+    puts(strcpy(thought,(strcat(hard_work,success))));
+    printf("\n\n");
+    int l=(strcmp(hard_work,success));
+    if(l=0){
+         printf("equal");
+        }
+     else{
+         printf("not equal");
+        }
 
+    char reverse=  strrev(success);
+    printf("%c",reverse);
 
-
-
-
-
-
-
-
-
-
-
-
-
+    return 0;
+}
