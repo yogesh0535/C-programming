e5a2fcfb1442baeb9c9490cd6ea5d537bf70ff8e
Yogesh bhardwaj <128271891+yogesh0535@users.noreply.github.com>
2024-01-02 21:49:19 +0530

    Update labmanual_practice.c

--- labmanual.c
+++ labmanual.c
@@ -1,98 +1,104 @@
-// #include <stdio.h>
-// void main()
-// {
-// int n, reverse = 0;
-// printf("Enter a number to reverse\n");
-// scanf("%d", &n);
-// while (n != 0)
-// {
-// reverse = reverse * 10;
-// reverse = reverse + n%10;
-// n
-// = n/10;
-// }
-// printf("Reverse of entered number is = %d\n", reverse);
-// 
-// return 0;
-// }
+#include <stdio.h>
+void main()
+{
+int n, reverse = 0;
+printf("Enter a number to reverse\n");
+scanf("%d", &n);
+while (n != 0)
+{
+reverse = reverse * 10;
+reverse = reverse + n%10;
+n
+= n/10;
+}
+printf("Reverse of entered number is = %d\n", reverse);
 
-// practice question number 19
-// #include<stdio.h>
-// int main(){
-    // int a=1;
-    // printf("odd numbers from 1 to 100 are: \n");
-    // for(a=1;a<=100;a++){
-    // if(a%2!=0){
-        // printf("%d",a);
-// printf("\n");
-    //  }
-    // }
-// 
-// printf("even numbers from 1 to 100 are: \n");
-// for(a=1;a<=100;a++){
-    // if(a%2==0){
-        // printf("%d",a);
-// printf("\n");
-    // }
-// }
-// 
-    // return 0;
-// }
-
-// practice  15 
-// #include<stdio.h>
-// int main(){
-    // int sum =0;
-    // int a;
-// printf("odd numbers from 1 to 100 are: \n");
-    // for(a=1;a<=100;a++){
-    // if(a%2!=0){
-        // printf("%d",a);
-// printf("\n");
-    //  }
-    //  sum=sum+a;
-// printf("sum of odd numbers is %d\n",sum);
-// 
-    // }
+return 0;
+}
 
 
-//  printf("even numbers from 1 to 100 are: \n");
-// for( a=1;a<=100;a++){
-    // if(a%2==0){
-        // printf("%d",a);
-// printf("\n");
-    // }
-// sum=sum+a;
-// printf("sum of even numbers is %d\n",sum);
 
-// } 
-//  return 0;
-// }
-// 
-// #include<stdio.h>
-// int main(){
-    // for(int i=0;i<=100;i++){
-        // printf("%d",i);
-        // printf("\n");
-    // int sum =0;
-    //  int sum=sum +i;
-    // printf("%d\n",sum);
-    // } 
-// 
-    // return 0;
-// }
 
-// convversion of celcius to feharanite 
-// #include<stdio.h>
-// int main(){
-    // float c;
-    // float f;
-//    scanf("%f",&c);
-//    f= (c*1.8)+32;
-// printf("%f",f);
-//  return 0;
-// }
-                        //comparision of 3 numbers
+#include<stdio.h>
+int main(){
+    int a=1;
+    printf("odd numbers from 1 to 100 are: \n");
+    for(a=1;a<=100;a++){
+    if(a%2!=0){
+        printf("%d",a);
+printf("\n");
+     }
+    }
+
+printf("even numbers from 1 to 100 are: \n");
+for(a=1;a<=100;a++){
+    if(a%2==0){
+        printf("%d",a);
+printf("\n");
+    }
+}
+ return 0;
+}
+
+
+
+#include<stdio.h>
+int main(){
+    int sum =0;
+    int a;
+printf("odd numbers from 1 to 100 are: \n");
+    for(a=1;a<=100;a++){
+    if(a%2!=0){
+        printf("%d",a);
+printf("\n");
+     }
+     sum=sum+a;
+printf("sum of odd numbers is %d\n",sum);
+
+    }
+printf("even numbers from 1 to 100 are: \n");
+for( a=1;a<=100;a++){
+    if(a%2==0){
+        printf("%d",a);
+printf("\n");
+    }
+sum=sum+a;
+printf("sum of even numbers is %d\n",sum);
+
+} 
+ return 0;
+}
+
+
+
+#include<stdio.h>
+int main(){
+    for(int i=0;i<=100;i++){
+        printf("%d",i);
+        printf("\n");
+    int sum =0;
+     int sum=sum +i;
+    printf("%d\n",sum);
+    } 
+
+    return 0;
+}
+
+
+                        // convversion of celcius to feharanite 
+#include<stdio.h>
+int main(){
+    float c;
+    float f;
+   scanf("%f",&c);
+   f= (c*1.8)+32;
+printf("%f",f);
+ return 0;
+}
+                       
+
+
+                                //comparision of 3 numbers
 
 #include<stdio.h>
 int main(){
@@ -116,128 +122,28 @@ int main(){
 
 
                         // switch statement using logical operators
-//#include<stdio.h>
-//#include<math.h>
-//int main(){
-//    int a,b,
-//    c;
-//    char operand;
-//    printf("enter the values of a and b\n");
-//    scanf("%d%d",&a,&b);
-//    printf("enter the operand '%' or '/' want to perform\n");
-//    scanf("%d",& operand);
-//    
-//    switch(operand){
-//    case '/':
-//           c=a/b;
-//             printf("%f",c);
-//            break;
+#include<stdio.h>
+#include<math.h>
+int main(){
+   int a,b,
+   c;
+   char operand;
+   printf("enter the values of a and b\n");
+   scanf("%d%d",&a,&b);
+   printf("enter the operand '%' or '/' want to perform\n");
+   scanf("%d",& operand);
+   
+   switch(operand){
+   case '/':
+          c=a/b;
+            printf("%f",c);
+           break;
+
+   case '%':
+   c=a%b;
+ printf("%f",c);
+ break;
+   }
+   return 0;
+}
 //
-//    case '%':
-//    c=a%b;
-//  printf("%f",c);
-//  break;
-//    }
-//
-//    return 0;
-//}
-//
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
